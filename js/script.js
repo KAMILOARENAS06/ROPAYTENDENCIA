d30d3094ecf1b596e0c8901d5358b8680a9de68f
KAMILOARENAS06 <168214702+KAMILOARENAS06@users.noreply.github.com>
2024-08-20 18:58:09 -0500

    Se sube archivos del proyecto

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Hamburger Menu Materialize
+document.addEventListener('DOMContentLoaded', function() {
+    const $sidenav = document.querySelectorAll('.sidenav');
+    const instances = M.Sidenav.init($sidenav);
+    // $(".dropdown-trigger").dropdown();
+});
+
